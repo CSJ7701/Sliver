7da18e3fac7edb5691cf19c430551588ab9bba72
Christian Johnson <c.ship.johnson@gmail.com>
2025-12-19 23:19:21 -0500

    Added missing content for sliver.el

--- sliver.el
+++ sliver.el
@@ -1,1 +1,11 @@
 ;;; sliver.el --- Modular emacs configuration management -*- lexical binding: t; -*-
+
+(require 'sliver-core)
+(require 'sliver-author)
+(require 'sliver-load)
+(require 'sliver-conditions)
+(require 'sliver-ui)
+
+(provide 'sliver)
+
+;;; sliver.el ends here
